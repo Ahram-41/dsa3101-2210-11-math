b033c17b2222fba3ae0cbca8ff8a905efa04c428
Phoebe <e0559489@u.nus.edu>
2022-10-21 19:04:31 +0800

    Modules Page

---
+++ Front-End/my-app/src/Components/Cards.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Cards.css'
+function Card({title,imageUrl,body}) {
+    return (
+        <div className= 'card-container'>
+            <div className="image-container">
+                <img src={imageUrl} alt='' />
+            </div>
+            <div className="card-content">
+                <div className="card-title">
+                    <h3>{title}</h3>
+                </div>
+                <div className="card-body">
+                    <p> {body}</p>
+                </div>               
+            </div>
+
+            <div className="btn">
+                <button>
+                    <a>View more</a>  
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Card
