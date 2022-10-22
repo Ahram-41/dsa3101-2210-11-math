1fb683e8ca239177cc5bfb0481640ec421ae4822
Phoebe <e0559489@u.nus.edu>
2022-10-22 20:04:55 +0800

    Sidebar and click to modules function

--- Front-End/my-app/src/Profile.js
+++ Front-End/my-app/src/Profile.js
@@ -1,17 +1,28 @@
 import React from "react";
 import "./Profile.css";
 import Card from './Components/Cards'
+import Navbar from './Components/Navbar.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 
 
 function Profile (){
     return (
-        <div classname= 'Profile'>
-            <Card 
-            title='ST3131'
-            imageUrl='https://www.stat.nus.edu.sg/wp-content/uploads/sites/8/2020/04/08235J.gif'
-            body='Regression Analysis'
-            />
+        <>
+        <Navbar />
+        <div>
+        <Card 
+        title='ST3131'
+        imageUrl='https://www.stat.nus.edu.sg/wp-content/uploads/sites/8/2020/04/08235J.gif'
+        body='Regression Analysis'
+        />
+
+
         </div>
+
+        </>
+
     );  
 }
 
