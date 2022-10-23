0aa437d41f365868e40a40e7e41032eaaf980a90
gargarteo <garrickteo@u.nus.edu>
2022-10-23 16:16:08 +0800

    Main.js

--- Front-End/flask-server/server.py
+++ Front-End/flask-server/server.py
@@ -1,14 +1,34 @@
-from flask import Flask
+import os
+from flask import Flask, request, session
+from werkzeug.utils import secure_filename
+from flask_cors import CORS
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('HELLO ! ')
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './uploads'
+CORS(app, expose_headers='Authorization')
 
-# Members API Route
-@app.route("/members")
 
-def members():
-    return {"members": ["Phoebe", "Kun Ming", "Daryl", "Garrick"]}
+@app.route('/upload', methods=['POST'])
+def fileUpload():
+    target = os.path.join(app.config['UPLOAD_FOLDER'])
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    logger.info("welcome to upload")
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    destination = "/".join([target, filename])
+    file.save(destination)
+    session['uploadFilePath'] = destination
+    response = "Uploaded"
+    return response
+
 
 if __name__ == "__main__":
-    app.run(debug =True)
-
-    
+    app.secret_key = os.urandom(24)
+    app.run(debug=True, port=5000)
---
+++ Front-End/my-app/src/Pages/Main.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import './Main.css'
+import Navbar from '../Components/Navbar.js';
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            imageURL: "",
+        };
+        this.handleUploadImage = this.handleUploadImage.bind(this);
+    }
+
+    handleUploadImage(ev) {
+        ev.preventDefault();
+    
+        const data = new FormData();
+        data.append('file', this.uploadInput.files[0]);
+        data.append('filename', this.fileName.value);
+    
+        fetch('http://localhost:5000/upload', { method: 'POST', body: data })
+        .then((response) => { response.json().then((body) => { 
+            this.setState({ imageURL: `http://localhost:5000/${body.file}` });
+          });
+        });
+      }
+
+    render() {
+        return (
+            <>
+            <Navbar />
+            <form onSubmit={this.handleUploadImage}>
+            <div class = "content">
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+              <input ref={(ref) => { this.uploadInput = ref; }} type="file" />
+            </div>
+            <br />
+            <div class = "content">
+              <input ref={(ref) => { this.fileName = ref; }} type="text" placeholder="Enter the desired name of file" class = "input" />
+            </div>
+            <br />
+            <br />
+            <div class = "content">
+              <button class = "btn">Upload</button>
+            </div>
+          </form>
+          </>
+        );
+      }
+}
+
+export default Main;
