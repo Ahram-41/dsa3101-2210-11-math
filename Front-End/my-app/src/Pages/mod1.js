8663f2c54c477caf1d22329ead67b3fd50fea9ee
Daryl <daryl.the.wang@gmail.com>
2022-10-23 17:03:05 +0800

    created select bar for mod1 page

--- Front-End/my-app/src/Pages/mod1.js
+++ Front-End/my-app/src/Pages/mod1.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import Navbar from '../Components/Navbar.js';
+import Select from 'react-select';
+import "./mod1.css";
+import { Link } from 'react-router-dom';
+
 
 function Mod1() {
+  const tobegraded = [
+    { value: 'Assignment 3', label: 'Assignment 3' },
+    { value: 'Mid-term exams', label: 'Mid-term exams' },
+    { value: 'Assignment 4', label: 'Assignment 4' }
+  ];
+  const graded = [
+    { value: 'Assignment 1', label: 'Assignment 1' },
+    { value: 'Assignment 2', label: 'Assignment 2' }
+  ];
+  const Spacer = require('react-spacer');
   return (
     <>
     <Navbar />
     <div className='mod1'>
-      <h1>Mod 1</h1>
+      <h2>Select the assignment to mark!</h2>
     </div>
+    <h3>To be graded:</h3>
+    <Select className="padded" options = {tobegraded} />
+    <Spacer size={40} />
+    <h3>Graded:</h3>
+    <Select className="padded" options = {graded} />
+    <Link style={{color: 'black'}} to ={'/mod2'}>
+    <div className="btn">
+      <button onclick= {'/mod2'}>
+        <a>Next Page</a>
+      </button>
+    </div>
+    </Link>
     </>
   );
 }
