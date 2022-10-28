9ac3ec13230237d7cee24b16b70a1be5e188abd3
Phoebe <e0559489@u.nus.edu>
2022-10-28 20:25:38 +0800

    Front page

--- Front-End/my-app/src/App.js
+++ Front-End/my-app/src/App.js
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";   
 import Profile from "./Profile.js";
-import Login from "./Login.js";
+import Login from "./Login.jsx";
 import Mod1 from './Pages/mod1';
 import Mod2 from './Pages/mod2';
 import Mod3 from './Pages/mod3';
@@ -13,6 +13,7 @@ import Mod5 from './Pages/mod5';
 import Mod6 from './Pages/mod6';
 import Main from './Pages/Main';
 import Annotate from './Pages/Annotate';
+import Home from "./Home";
 
 function App() {
     return (
@@ -31,6 +32,7 @@ function App() {
                 <Route path='/Main' element={<Main />} />
                 <Route path='/Annotate' element={<Annotate />} />
                 <Route path='/testpdf' element={<testpdf/>} />
+                <Route path='/' element={<Home />} />
             </Routes>
         </Router>
         </div>
